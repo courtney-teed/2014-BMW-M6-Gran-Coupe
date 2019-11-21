cf16e76852c403368103018c7c10d36dd304bf57
courtney-teed <42760270+courtney-teed@users.noreply.github.com>
2019-11-21 15:11:35 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
+
+
+
+$('#aniimated-thumbnials').lightGallery({
+    thumbnail:true
+});
